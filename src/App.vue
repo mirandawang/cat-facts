d65e1d8994bac4daf57e270e75e1ea0555672ff6
Miranda Wang <msw3bb@virginia.edu>
2018-01-24 09:00:36 -0500

    Add cat facts data and display all of them

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <CatFacts/>
+    <CatFacts v-bind:facts="catfacts" />
   </div>
 </template>
 
@@ -10,8 +10,23 @@ import CatFacts from './components/CatFacts';
 export default {
   name: 'App',
   components: {
-    CatFacts,
+    CatFacts
   },
+  data () {
+    return {
+      catfacts: [
+        'The ancient Egyptians were the first to tame the cat (in about 3000 BC), and used them to control pests.',
+        'Cats share 95.6% of their DNA with tigers.',
+        'Cats are asleep for 70% of their lives.',
+        'Cat kidneys are super efficient, they can rehydrate by drinking seawater.',
+        'Kittens who are taken along on short, trouble-free car trips to town tend to make good passengers when they get older.', 'They get used to the sounds and motions of traveling and make less connection between the car and the visits to the vet.',
+        'Cats can jump approximately seven times their height',
+        'A group of kittens is called a kindle, and a group of cats is called a clowder',
+        'A cat has 230 bones — even more than a human!',
+        'Cats greet one another by touching noses - boop!'
+      ]
+    }
+  }
 };
 </script>
 
--- src/components/CatFacts.vue
+++ src/components/CatFacts.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>Random Cat Facts</h1>
-    <p>This is a cat fact</p>
+    <p>
+      {{ facts }}
+    </p>
     <button>Randomize!</button>
   </div>
 </template>
 
 <script type="text/javascript">
 
-export default {};
+export default {
+  props: ['facts']
+};
 
 </script>
 
